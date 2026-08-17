7b5c5b40f8af0d0a81d9950e3bcb5249c573946f
VocaFlow Developer <dev@vocaflow.app>
2026-08-17 18:18:04 +0700

    feat: Add PWA Manifest, Service Worker and native Android App Install trigger

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'vocaflow-pwa-v1.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './xlsx.full.min.js',
+  './manifest.json',
+  './icons/Icon-192.png',
+  './icons/Icon-512.png',
+  './icons/Icon-maskable-192.png',
+  './icons/Icon-maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
