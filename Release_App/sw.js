c6e926f270995850f4953f90cfc155d5e08fab6e
VocaFlow Developer <dev@vocaflow.app>
2026-08-19 11:52:17 +0700

    feat(branding): Unify VocaFlow app icon across Windows window titlebar, taskbar, PWA and header, bump to v0.0.9.13

--- Release_App/sw.js
+++ Release_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.0.9.12';
+const CACHE_NAME = 'vocaflow-pwa-v0.0.9.13';
 const ASSETS = [
   './',
   './index.html',
--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c6f33ed30e4eef0d62b05cd89c49c6b401ad0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8d77ba452798fe4b6ee5671761dbdefac8ff6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
