057f9f06a02d00f57c8669f0d52857c6642cb11b
VocaFlow Developer <dev@vocaflow.app>
2026-08-24 20:29:48 +0700

    fix(speaking): Release v0.10.6o - Next locking, history state, scoring harmony & mic lifecycle

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe92feb6a2e2d459090d3b4665979ff2693ce1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a9cb2a9f30b65ee89a7379301eeed79568934a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.6n';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.6o';
 const ASSETS = [
   './',
   './index.html',
