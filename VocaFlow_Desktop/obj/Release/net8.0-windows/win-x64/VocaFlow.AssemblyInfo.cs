12214c659205e3540ccfe624b231c709c092c9a0
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 00:15:58 +0700

    fix(ui): Release v0.10.8-beta-2.4 - Unified Golden-Pink VIP Shimmer across Themes

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf88946f50585373275417f43ffe0f5f86587ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e1e62d1eae1fe4ff0d882a4edab5e77ea40739")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
