d8267228e135d72b1bc1e2a90ac998a78eca0737
VocaFlow Developer <dev@vocaflow.app>
2026-08-25 21:54:51 +0700

    fix(auth): Release v0.10.7g-9 - Complete Logout Memory Wipeout & Self-Follow Purge

--- Release_App/sw.js
+++ Release_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.7g-8';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.7g-9';
 const ASSETS = [
   './',
   './index.html',
--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a6b62ddfa5c0b69b6cf9aa15a06d3295e8262c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61995f259bfc2d2e54e90bad9fd2d815b69b8d01")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
