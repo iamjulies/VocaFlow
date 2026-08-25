cd82c1dbb4585fb51ff6dcf1e1a3bf53133c94fc
VocaFlow Developer <dev@vocaflow.app>
2026-08-25 13:20:44 +0700

    fix(social): Release v0.10.7d-1 - Fix Real-time Target Follower Count Increment

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26c8e4c59af93b2eb3d55e6d5c077bbec57c10f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f274d9c12b1455c1a90aa68ede3170b156e24a2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
