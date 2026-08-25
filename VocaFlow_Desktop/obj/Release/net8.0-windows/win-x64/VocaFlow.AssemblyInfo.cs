4bf88946f50585373275417f43ffe0f5f86587ea
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 00:03:01 +0700

    fix(ui): Release v0.10.8-beta-2.2 - Natural Crown Emoji & Vibrant Light Theme Polish

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9e15d727ad4e5f6f23d549a33fba9a9ace1470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d23891feb8a9c4b4f59abf5ce37d861c4c098b5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-2.1';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-2.2';
 const ASSETS = [
   './',
   './index.html',
