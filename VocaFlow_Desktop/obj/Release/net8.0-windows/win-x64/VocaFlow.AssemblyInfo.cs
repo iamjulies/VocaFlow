10dae70ec686561686b50e25c7abaa6a89bb7e29
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 00:56:48 +0700

    fix(ui): Release v0.10.8-beta-2.11 - Separated Dual-Layer VIP Architecture

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6d971de532c76b0d237c6199912c125dac77b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4132d9546fe0d697ad3479ee0c8c0b2803c0443a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-2.10';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-2.11';
 const ASSETS = [
   './',
   './index.html',
