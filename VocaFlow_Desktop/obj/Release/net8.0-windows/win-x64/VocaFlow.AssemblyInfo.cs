316db9482b7da1f9a6d81db48a08ccdc200aab0d
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 02:18:59 +0700

    feat(vip): Release v0.10.8-beta-3.7 - Realtime Global VIP Registry, Guest Glow Fix & Uncut UID Syntax

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7def258e05a40abf26976401138ef41b39dfbf39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801d78edf17d6bf4ffecf20580e57d0efc44ac7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-3.6';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-3.7';
 const ASSETS = [
   './',
   './index.html',
