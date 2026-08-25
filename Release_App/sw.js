2faa9a1a03e4c60f27beacec9905137599a1a321
VocaFlow Developer <dev@vocaflow.app>
2026-08-25 15:27:52 +0700

    fix(ai-mentor): Release v0.10.7g-5 - Mini Quiz Interaction Cache & Click Resolution Hotfix

--- Release_App/sw.js
+++ Release_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.7g-4';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.7g-5';
 const ASSETS = [
   './',
   './index.html',
--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9cda7903c544069845307221e9a6ab6fa048ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a323c22006a08f250dc6fef1d4378952665a65")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
