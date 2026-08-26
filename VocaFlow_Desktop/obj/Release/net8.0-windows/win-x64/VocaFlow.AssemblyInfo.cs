7ae712404af8ec24403324730c7ecb25c03a7dd6
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 13:34:55 +0700

    feat(referral): Release v0.10.8-beta-6 - Referral Program (+1 VIP Day Reward) & ads.txt Verification

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718dfd60269f668981f7604eb3cdca956570b892")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0aa7c8c24e712012c4cb43913d1ffec7071f726")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
