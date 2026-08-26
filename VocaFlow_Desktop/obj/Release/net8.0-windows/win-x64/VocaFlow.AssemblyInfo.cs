e0d8c3a8e23e41ea9047b4691eb02116f2589a21
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 18:14:47 +0700

    feat(refinement): Release v0.10.8-beta-6.5 - 7-Point Refinements & Cloud Perfection (Build 162)

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e72deaf75cd70bb856fd9efedda3aa431e63ad7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8b3540a1ad3f6b03058c82b043b231eadd107e")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
