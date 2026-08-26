dc23857be6990121e78e4921aa419713800b53f6
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 12:17:01 +0700

    fix(lucky-wheel): Release v0.10.8-beta-5.2 - VIP Expiration Extension, No-Scrollbar Wheel & VietQR Spin Packages

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+134f3dd89c9f5b0f292a4a3ac0bb51f872260240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a878243286abf90c0f3079cf675166c1ce61af")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
