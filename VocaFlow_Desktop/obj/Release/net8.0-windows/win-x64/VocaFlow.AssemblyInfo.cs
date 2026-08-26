c67d4e17b83f11a4c1dce5832ddc430ec5c1f551
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 12:41:59 +0700

    feat(shop): Release v0.10.8-beta-5.4 - Move Gift Code to Top, Rewarded Ad Cooldown Fix & Spin Reward Spec

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc23857be6990121e78e4921aa419713800b53f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9144ab7d92ae084758146cb252e9e4bec50825")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
