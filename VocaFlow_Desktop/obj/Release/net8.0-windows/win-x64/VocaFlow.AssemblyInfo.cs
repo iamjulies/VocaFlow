a23e6ea9a1e6a1ebb98c8f1072c6d2049af9fe4b
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 11:37:54 +0700

    feat(vip): Release v0.10.8-beta-4.2 - Responsive Segmented VIP Plan Switcher

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1107466d7a7933c837a55ae7e3a0b202df08fb54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bff82212317b6876f6137175c3d72e7448398f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
