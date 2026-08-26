7377be70b377e38dfa8dc087c5507fabb1809226
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 13:59:25 +0700

    fix(ui): Release v0.10.8-beta-6.2 - Clean AdSense Placeholders, Vibrant Cooldown UI & Root Sync

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae712404af8ec24403324730c7ecb25c03a7dd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21d01fc9fb92a8f6e6e525a97213bc751b7a241")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-6.1';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-6.2';
 const ASSETS = [
   './',
   './index.html',
