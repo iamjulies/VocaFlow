718dfd60269f668981f7604eb3cdca956570b892
VocaFlow Developer <dev@vocaflow.app>
2026-08-26 13:05:15 +0700

    feat(ads): Release v0.10.8-beta-5.5 - Integrate Google AdSense ca-pub-1343250003116465 & Clickable Ad Card

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9144ab7d92ae084758146cb252e9e4bec50825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67d4e17b83f11a4c1dce5832ddc430ec5c1f551")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-5.4';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-5.5';
 const ASSETS = [
   './',
   './index.html',
