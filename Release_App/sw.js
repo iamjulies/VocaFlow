e2193d2665b3f3a49108b1700b75c54843d6200f
VocaFlow Developer <dev@vocaflow.app>
2026-08-29 01:21:38 +0700

    feat: Release v0.10.8-beta-8.11 (Build 190) - Local date timezone alignment, enlarged flow icons and continuous connected stream paths

--- Release_App/sw.js
+++ Release_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-8.10';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-beta-8.11';
 const ASSETS = [
   './',
   './index.html',
--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2396fc3bf0172cd4ac3545a374c6a4548dea0957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb5fc9bf9879fac64199c88943a60ca93c7054b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
