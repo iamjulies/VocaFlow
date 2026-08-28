64862cc87cc7af781ad3bdd937c07541c9ca6020
VocaFlow Developer <dev@vocaflow.app>
2026-08-28 22:48:24 +0700

    feat: Release v0.10.8-beta-8.2 (Build 181) - Redesigned Achievement Pill Filter Tabs and UI Polish

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20e9f7b1fdad2e2449af689682cdbc1092a3f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80debba5d82a521ac6fe875680867dd4a9790608")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
