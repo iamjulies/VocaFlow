acb6c6ae5fcdc45c912a048d4ca86da92e412108
VocaFlow Developer <dev@vocaflow.app>
2026-08-29 13:36:18 +0700

    feat: Release v0.10.8-alpha-10 (Build 205) - VIP Multi-Key Pool (x3 Quota), Tiered Failover and 2-way cloud sync

--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9dff41f38fb7aedd12fb885006e01a477f647e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec49929533ae15aaebecfe932f12ec13b505d7a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
