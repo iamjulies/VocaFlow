b0cddc6a8ed2609b980a63d11189ad18dcf1168c
VocaFlow Developer <dev@vocaflow.app>
2026-08-29 16:25:37 +0700

    feat: Release v0.10.8-alpha-10.8 (Build 213) - 1-Click Smart Paste Banking SMS & Instant Reconciler

--- Release_App/sw.js
+++ Release_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-alpha-10.7';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-alpha-10.8';
 const ASSETS = [
   './',
   './index.html',
--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1860ccf985ddbbf970510fd05e9ee790771b98d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a756468efce83d710dc844e78beec8cd2ad0a655")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
