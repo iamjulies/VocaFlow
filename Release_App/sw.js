85d777500579a8f2a68774943a0b762b11eaacc0
VocaFlow Developer <dev@vocaflow.app>
2026-08-29 13:57:59 +0700

    fix: Release v0.10.8-alpha-10.3 (Build 208) - Global VIP & storage key architecture rebuild for complete stability

--- Release_App/sw.js
+++ Release_App/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vocaflow-pwa-v0.10.8-alpha-10.2';
+const CACHE_NAME = 'vocaflow-pwa-v0.10.8-alpha-10.3';
 const ASSETS = [
   './',
   './index.html',
--- VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
+++ VocaFlow_Desktop/obj/Release/net8.0-windows/win-x64/VocaFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e0d6ca3970013726535e9726c42ceea9b05c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97acdbd43cce5dd20350a79aa9171ba8d2093723")]
 [assembly: System.Reflection.AssemblyProductAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VocaFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
